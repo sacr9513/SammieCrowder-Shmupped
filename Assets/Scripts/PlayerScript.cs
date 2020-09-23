72ead17cf00d8528ef4bcada8cb714e405feca9b
Sammie <sacr9513@colorado.edu>
2020-09-23 17:05:36 -0600

    Initial commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+    private float     xPos;
+    public float      speed = .05f;
+    public float      leftWall, rightWall;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            if (xPos > leftWall) {
+                xPos -= speed;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            if (xPos < rightWall) {
+                xPos += speed;
+            }
+        }
+
+        transform.localPosition = new Vector3(xPos, transform.position.y, 0);
+    }
+}
+
